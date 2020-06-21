8ee10735fb72fdb945af32302aca7c7b08fed365
Roshan Bashyam <bashyamroshan@hotmail.com>
2020-06-21 13:30:37 +0530

    Added Files for Tab Navigation

--- App.js
+++ App.js
@@ -1,19 +1,9 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+//Pulling AppNavigator from the navigation Folder
+import AppNavigator from './src/navigation/AppNavigator'
 
-export default function App() {
+export default App = () => {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
-  );
+    <AppNavigator/>
+  )
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
