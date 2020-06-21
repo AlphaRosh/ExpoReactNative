c59015254729bd584dfa7c6ce931c967e7e06d9e
Roshan Bashyam <bashyamroshan@hotmail.com>
2020-06-22 00:01:08 +0530

    Added logic for navigation

--- App.js
+++ App.js
@@ -1,14 +1,45 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
+//Navigation Format
+// - AppSwitchNavigation
+//    - WelcomeScreen
+//      -Login button
+//      -Sign up Button
+//    - AppDrawerNavigation
+//      - Dashboard - DashboardNavigation(needed for headers and to change the headers based on the tab)
+//    - DashboardTabNavigation
+//      - Tab-1 - FeedStack
+//      - Tab-2 - ProfileStack
+//      - Tab-3 - SettingsStack
+//    - Other Files 
+
+//Import SwitchNavigator
+import {
+  createSwitchNavigator,
+  createAppContainer
+ 
+} from "react-navigation";
+
+//importing screens
+import WelcomeScreen from './screens/WelcomeScreen'
+import DashboardScreen from './screens/DashboardScreen'
+
+
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
+   <AppContainer/>
   );
 }
 
+//createSwitchNavigation
+const AppSwitchNavigator = createSwitchNavigator({
+  Welcome: { screen: WelcomeScreen },
+  Dashboard: { screen: DashboardScreen }
+
+})
+
+const AppContainer = createAppContainer(AppSwitchNavigator);
 const styles = StyleSheet.create({
   container: {
     flex: 1,
